336d192fd2839aff97f481f0fb2b841fe1d7db09
Yujeong Choung <yjchoung10@gmail.com>
2022-04-11 21:51:48 +0900

    initial commit

---
+++ prepare/front/pages/index.js
@@ -0,0 +1,10 @@
+// next는 imoprt React가 불필요
+// import React from 'react'
+
+const Home = () => {
+  return <div>Hello, Next!</div>;
+};
+
+export default Home;
+
+// pages 폴더 안에 있어야만 코드 스필릿팅이 가능하기 때문에 무조건 폴더이름은 pages여야한다
