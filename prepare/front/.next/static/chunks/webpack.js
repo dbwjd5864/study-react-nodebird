80950382e3a87bb4d37531ab306d9ec0b0b77c3f
Yujeong Choung <yjchoung10@gmail.com>
2022-04-25 21:50:41 +0900

    포스트 카드와 댓글창 생성

--- prepare/front/.next/static/chunks/webpack.js
+++ prepare/front/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fb37957b9a9d59e4a0ac";
+/******/ 	var hotCurrentHash = "f69afb118faf49e7d281";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- prepare/front/components/PostCard.js
+++ prepare/front/components/PostCard.js
@@ -1,7 +1,127 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Button, Card, Popover, Avatar, List, Comment } from 'antd';
+import {
+  RetweetOutlined,
+  EllipsisOutlined,
+  HeartOutlined,
+  HeartTwoTone,
+  MessageOutlined,
+} from '@ant-design/icons';
+import PropTypes from 'prop-types';
+import PostImages from './PostImages';
+import CommentForm from './CommentForm';
 
-const PostCard = () => {
-  return <div>PostCard</div>;
+const PostCard = ({ post }) => {
+  // const { me } = useSelector((state) => state.user);
+  // optional chanining
+  // const id = me?.id;
+  const id = useSelector((state) => state.user.me?.id);
+
+  const [liked, setLiked] = useState(false);
+  const [commentFormOpened, setCommentFormOpened] = useState(false);
+
+  const onClickUpdate = () => {};
+
+  const onRemovePost = () => {};
+
+  const onToggleLike = useCallback(() => {
+    setLiked((prev) => !prev);
+  }, []);
+
+  const onToggleComment = useCallback(() => {
+    setCommentFormOpened((prev) => !prev);
+  }, []);
+
+  const onLike = () => {};
+
+  const onUnlike = () => {};
+
+  return (
+    <div style={{ marginBottom: 20 }}>
+      <Card
+        cover={post.Images[0] && <PostImages images={post.Images} />}
+        // jsx안에 배열값에는 항상 키값이 필요
+        actions={[
+          <RetweetOutlined key="retweet" />,
+          liked ? (
+            <HeartTwoTone
+              twoToneColor="#eb2f96"
+              key="heart"
+              onClick={onToggleLike}
+            />
+          ) : (
+            <HeartOutlined key="heart" onClick={onToggleLike} />
+          ),
+          <MessageOutlined key="comment" onClick={onToggleComment} />,
+          <Popover
+            key="more"
+            content={
+              id && post.User.id === id ? (
+                <>
+                  {!post.RetweetId && (
+                    <Button onClick={onClickUpdate}>수정</Button>
+                  )}
+                  <Button
+                    type="danger"
+                    // loading={removePostLoading}
+                    onClick={onRemovePost}
+                  >
+                    삭제
+                  </Button>
+                </>
+              ) : (
+                <Button>신고</Button>
+              )
+            }
+          >
+            <EllipsisOutlined />
+          </Popover>,
+        ]}
+      >
+        <Card.Meta
+          avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+          title={post.User.nickname}
+          description={post.content}
+        />
+      </Card>
+      {commentFormOpened && (
+        <div>
+          <CommentForm post={post} />
+          <List
+            header={`${post.Comments.length}개의 댓글`}
+            itemLayout="horizontal"
+            dataSource={post.Comments}
+            renderItem={(item) => (
+              <li>
+                <Comment
+                  author={item.User.nickname}
+                  avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                  content={item.content}
+                />
+              </li>
+            )}
+          />
+        </div>
+      )}
+      {/* <CommnetForm /> */}
+      {/* <Comments /> */}
+    </div>
+  );
+};
+
+PostCard.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number,
+    User: PropTypes.object,
+    content: PropTypes.string,
+    createdAt: PropTypes.string,
+    Comments: PropTypes.arrayOf(PropTypes.object),
+    Images: PropTypes.arrayOf(PropTypes.object),
+    Likers: PropTypes.arrayOf(PropTypes.object),
+    RetweetId: PropTypes.number,
+    Retweet: PropTypes.objectOf(PropTypes.any),
+  }).isRequired,
 };
 
 export default PostCard;
