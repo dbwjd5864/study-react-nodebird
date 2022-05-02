18ec68e4fc861a67602dfd2eb4faf7a18372f954
Yujeong Choung <yjchoung10@gmail.com>
2022-05-02 22:49:07 +0900

    take, throttle

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -1,4 +1,13 @@
-import { all, fork, take, call, put } from 'redux0-saga/effects';
+import {
+  all,
+  fork,
+  take,
+  delay,
+  takeEvery,
+  takeLatest,
+  call,
+  put,
+} from 'redux0-saga/effects';
 import axios from 'axios';
 
 function logInAPI(data) {
@@ -11,7 +20,8 @@ function* login(action) {
   try {
     // 서버의 결고 요청값을 받음
     // 무조건 결과값을 받아와야하기때문에 fork가 아닌 call
-    const result = yield call(logInAPI, action.data);
+    // const result = yield call(logInAPI, action.data);
+    yield delay(1000);
     yield put({ type: 'LOG_IN_SUCCESS', data: result.data });
   } catch (err) {
     yield put({ type: 'LOG_IN_FAILURE', data: err.response.data });
@@ -24,7 +34,8 @@ function logOutAPI() {
 
 function* logOut() {
   try {
-    const result = yield call(logOutAPI);
+    // const result = yield call(logOutAPI);
+    yield delay(1000);
     yield put({ type: 'LOG_OUT_SUCCESS', data: result.data });
   } catch (err) {
     yield put({ type: 'LOG_OUT_FAILURE', data: err.response.data });
@@ -47,15 +58,31 @@ function* addPost(action) {
 // LOG_IN 이라는 액션이 시작될때까지 기다림을 의미
 function* watchLogin() {
   // LOG_IN이라는 액션이 시작되면 login이 시작됨
-  yield take('LOG_IN_REQUEST', login);
+  // 딱 한번밖에 이벤트 리스너가 작동을 안하기 때문에 while로 감싸주는 것이 필요
+  // 하지만 직관적이지 않기 때문에 보통은 takeEvery를 사용
+  // while(true){
+  //   yield take('LOG_IN_REQUEST', login);
+  // }
+
+  // while take는 동기적으로 동작하지만 takeEvery는 비동기적으로 동작한다는 차이가 존재한다
+  // yield takeEvery('LOG_IN_REQUEST', login);
+
+  // 앞에 실수로 눌렀던 것은 무시되고 마지막것만 실행된다
+  // 하지만 요청은 그대로 가고 응답만 취소하는 것이기 때문에 backend단에서 데이터 처리시 유의해야함
+  yield takeLatest('LOG_IN_REQUEST', login);
+  //첫번째 것만은 takeLeading
+
+  // throttle
+  // 2초동안 요청은 딱 한번만 존재할 수 있음을 의미
+  // yield throttle('LOG_IN_REQUEST', login, 2000)
 }
 
 function* watchLogout() {
-  yield take('LOG_OUT_REQUEST', logOut);
+  yield takeEvery('LOG_OUT_REQUEST', logOut);
 }
 
 function* watchAddPost() {
-  yield take('ADD_POST_REQUEST', addPost);
+  yield takeEvery('ADD_POST_REQUEST', addPost);
 }
 
 // rootSaga를 만들어두고 거기에 사용하고싶은 비동기 액션들을 넣어준다
