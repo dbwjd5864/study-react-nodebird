e7330f4cc3147494d31b7a7e866ce9ce3e50f78d
yujeong-choung <yujeong.choung@gmail.com>
2022-07-29 09:58:41 +0900

    로그인 구현 중

--- prepare/back/routes/user.js
+++ prepare/back/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
-const { User } = require('../models');
+const { User, Post } = require('../models');
 
 const router = express.Router();
 
@@ -26,7 +26,21 @@ router.post('login', (req, res, next) => {
                 console.error(loginErr);
                 return next(loginErr);
             }
-            return res.json(user);
+            const fullUserWithoutPassword = await User.findOne({
+                where: {id: user.id},
+                attributes : {exclude: ['password']},
+                // attributes: ['id','nickname','email'],
+                include: [{
+                    model: Post,
+                }, {
+                    model: User,
+                    as: "Followings",
+                },{
+                    model: User,
+                    as: "Followers",
+                }]
+            })
+            return res.status(200).json(user);
         })
     })(req, res, next);
 });
