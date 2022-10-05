cd53c9cfd0d5c9e9bc53ab395cd1aa7d9d77db21
Yujeong Choung <yujeong.choung@gmail.com>
2022-10-05 23:23:01 +0900

    feat: FollowList 컴포넌트 최적화

--- prepare/front/components/FollowList.js
+++ prepare/front/components/FollowList.js
@@ -1,30 +1,51 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
 import { List, Button, Card } from 'antd';
 import { StopOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
 
-const FollowList = ({ header, data }) => {
+const StyledList = styled(List)`
+    margin-bottom: 20 
+`;
+
+const StyledListItem = styled(List.Item)`
+    margin-top: 20 
+`;
+
+const ButtonContainer = styled.div`
+    text-align: center;
+    margin: 10px 0
+`;
+
+function FollowList({ header, data }) {
+  const grid = useMemo(() => ({ gutter: 4, xs: 2, md: 3 }), []);
+
   return (
-    <List
-      style={{ marginBottom: 20 }}
-      grid={{ gutter: 4, xs: 2, md: 3 }}
+    <StyledList
+      grid={grid}
       size="small"
       header={<div>{header}</div>}
-      loadMore={
-        <div style={{ textAlign: 'center', margin: '10px 0' }}>
+      loadMore={(
+        <ButtonContainer>
           <Button>더 보기</Button>
-        </div>
-      }
+        </ButtonContainer>
+      )}
       bordered
       dataSource={data}
       renderItem={(item) => (
-        <List.Item style={{ marginTop: 20 }}>
+        <StyledListItem>
           <Card actions={[<StopOutlined key="stop" />]}>
             <Card.Meta description={item.nickname} />
           </Card>
-        </List.Item>
+        </StyledListItem>
       )}
     />
   );
+}
+
+FollowList.propTypes = {
+  header: PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired,
 };
 
 export default FollowList;
