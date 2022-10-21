9efaf094aff38d28106e8d28454f3c43dfd525c9
Yujeong Choung <yujeong.choung@gmail.com>
2022-10-21 23:42:53 +0900

    feat: useSWR 셋팅

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import Head from 'next/head';
 import Router from 'next/router';
 
+import useSWR from 'swr';
 import axios from 'axios';
 import { END } from 'redux-saga';
 import NicknameEditForm from '../components/NicknameEditForm';
@@ -12,8 +13,25 @@ import AppLayout from '../components/AppLayout';
 import wrapper from '../store/configureStore';
 import { LOAD_MY_INFO_REQUEST } from '../reducers/user';
 
+const fetcher = (url) => axios.get(url, { withCredentials: true }).then((result) => result.data);
+
 function Profile() {
   const { me } = useSelector((state) => state.user);
+
+  // data와 error 둘다 없으면 로딩 중
+  // 둘 중 하나라도 있으면 성공이거나 실패
+  const { data: followersData, error: followerError } = useSWR(`${backUrl}/user/followers?limit=${followersLimit}`, fetcher);
+  const { data: followingsData, error: followingError } = useSWR(`${backUrl}/user/followings?limit=${followingsLimit}`, fetcher);
+
+  // useSWR로 대체 가능하다
+  // useEffect(() => {
+  //   dispatch({
+  //     type: LOAD_FOLLOWERS_REQUEST,
+  //   });
+  //   dispatch({
+  //     type: LOAD_FOLLOWINGS_REQUEST,
+  //   });
+  // }, []);
 
   useEffect(() => {
     if (!(me && me.id)) {
@@ -25,6 +43,12 @@ function Profile() {
     return null;
   }
 
+  // hooks는 항상 실행횟수가 같아야하기 때문에 return이 hook보다 위에 있을 수 없다.
+  if (followerError || followingError) {
+    console.error(followerError || followingError);
+    return <div>팔로잉/팔로워 로딩 중 에러가 발생합니다.</div>;
+  }
+
   return (
     <>
       <Head>
@@ -32,8 +56,8 @@ function Profile() {
       </Head>
       <AppLayout>
         <NicknameEditForm />
-        <FollowList header="팔로잉" data={me.Followings} />
-        <FollowList header="팔로워" data={me.Followers} />
+        <FollowList header="팔로잉" data={followingsData} />
+        <FollowList header="팔로워" data={followersData} />
       </AppLayout>
     </>
   );
